806f0195c6e05236d320003027c3f8ff1d84e438
Yu-no <Yu-no@DESKTOP-B6BDGE9>
2019-01-29 14:43:36 +0900

    vv

---
+++ src/main/java/com/lafresh/mapper/MainMapper.java
@@ -0,0 +1,11 @@
+package com.lafresh.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.lafresh.vo.SensorDataVO;
+
+@Mapper
+public interface MainMapper {
+	void insertTest(String test);
+	void insertSensorData(SensorDataVO sensorData);
+}
---
+++ src/main/java/com/lafresh/vo/SensorDataVO.java
@@ -0,0 +1,15 @@
+package com.lafresh.vo;
+
+public class SensorDataVO {
+	
+	private int temp;
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+
+}
