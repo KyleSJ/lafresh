9f9b6a1ea2aedcf2be7bc9d37f9f161b6489f863
Yu-no <Yu-no@DESKTOP-B6BDGE9>
2019-01-29 14:43:04 +0900

    cc

--- src/main/java/com/lafresh/DemoApplication.java
+++ src/main/java/com/lafresh/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.lafresh;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.lafresh.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lafresh/controller/mainController.java
+++ src/main/java/com/lafresh/controller/mainController.java
@@ -1,15 +1,22 @@
 package com.lafresh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lafresh.Service.MainService;
 
 @Controller
 public class mainController {
+	
+	@Autowired
+	MainService mainService;
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String viewMain() {
+		mainService.createTest("test ok");
 		return "test";
 	}
-	
 }
